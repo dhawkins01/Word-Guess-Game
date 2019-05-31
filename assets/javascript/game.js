9eacbaef64a35d0dc3ad2a960dfd5d0c8cc74963
Dave Hawkins <dhawkins01@gmail.com>
2019-05-30 23:20:51 -0400

    setting my vars in the javascript file

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// Global Variables..............
+
+// Array that will hold all the possible words
+
+var  produce = [ "banana", "apple", "avocado", "orange", "grape", "strawbery", "peach", "potatoe" ];
+
+// set my counters
+var wins = 0;
+var guesses = 20;
+
+
+
